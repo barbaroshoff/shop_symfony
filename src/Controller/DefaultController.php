382b9b6f668f1189d2cd41a6a115f800aef9d1b0
barbaroshoff <e.barbaros@unifun.com>
2022-10-11 12:03:23 +0300

    created edit product

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,19 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\EditProductFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="home_page")
+     * @Route("/", methods="GET", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('main/default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        $entityManager = $this->getDoctrine()->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+        dd($productList);
+        return $this->render('main/default/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/edit-product/{id}", methods="GET|POST", name="product-edit", requirements={"id"="\d+"})
+     * @Route("/add-product", methods="GET|POST", name="product_add_old", requirements={"id"="\d+"})
+     */
+    public function editProduct(Request $request, int $id = null): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        if($id) {
+            $product = $entityManager->getRepository(Product::class)->find($id);
+        } else {
+            $product = new Product();
+        }
+
+        $form = $this->createForm(EditProductFormType::class, $product);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product-edit', ['id' => $product->getId()]);
+        }
+
+        return $this->render('main/default/edit_product.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
